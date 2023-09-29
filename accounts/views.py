564409780a33e97cf28cfa4d3f45d58e1ea8f0bc
danial sadri <danielsadri01@gmail.com>
2023-09-29 12:35:34 +0330

    add ProfileView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class ProfileView(APIView):
+    serializer_class = UserSerializer
+
+    def get(self, request: Request):
+        user_serializer = UserSerializer(instance=request.user)
+        return Response(data=user_serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request: Request):
+        user_serializer = UserSerializer(instance=request.user, data=request.data, partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(data=user_serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(data=user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
