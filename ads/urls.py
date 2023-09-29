2c467a798820f19d56b15f324e7935083ded8a8e
danial sadri <danielsadri01@gmail.com>
2023-09-29 19:42:26 +0330

    add url

--- ads/urls.py
+++ ads/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('ad_update/<int:ad_id>/', views.AdUpdateView.as_view()),
     path('ad_delete/<int:ad_id>/', views.AddDeleteView.as_view()),
     path('search/', views.AdSearchView.as_view()),
+    path('search/', views.AdSearchView.as_view()),
 ]
